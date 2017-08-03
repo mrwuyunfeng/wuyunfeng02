38057774f7eddc7bf7b94e4897099fcb1aa56c6a
mrwuyunfeng <913821317@qq.com>
2017-08-03 23:36:36 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/testHelloWorld.java
@@ -0,0 +1,13 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/8/3 0003.
+ */
+public class testHelloWorld {
+    @Test
+    public void sayHello(){
+        System.out.println("hello");
+    }
+}
